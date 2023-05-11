984d6f199d2e9cf40a9adbf95974a399bd677d40
John Wilson <johnwilson7mc@gmail.com>
2023-05-11 16:01:47 +0400

    fjdk

--- src/components/select/select.js
+++ src/components/select/select.js
@@ -1,13 +1,17 @@
 document.addEventListener("click", (e) => {
     const select = document.querySelector(".select.active");
     const selectButton = e.target.closest("[data-toggle='select']");
-    const selectMenu = e.target.closest(".select-menu");
     const selectItem = e.target.closest(".select-item");
 
-    if (selectButton) selectButton.closest(".select").classList.add("active");
-    if (select && !selectMenu) select.classList.remove("active");
-    if (selectItem) {
-        select.querySelector(".select-name").innerText = selectItem.innerText;
-        select.classList.remove("active")
+    const setActiveItem = ()=>{
+        const wrapper = selectItem.closest(".select").querySelector(".btn > .wrapper");
+        wrapper.innerHTML = selectItem.innerHTML;
+        const title = wrapper.querySelector(".select-item-title");
+        if (title) title.classList.replace("select-item-title", "select-toggle-title");
+        if(wrapper.classList.contains('placeholder'))wrapper.classList.remove('placeholder')
     }
+
+    if (selectButton) selectButton.closest(".select").classList.add("active");
+    if (select) select.classList.remove("active");
+    if (selectItem) setActiveItem()
 });
